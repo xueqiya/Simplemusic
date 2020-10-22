9c2379639fdeee65b693b711fc35219762e47fa8
xueqi <xueqi@zjmedia.net>
2020-10-22 12:28:46 +0800

    feat: 添加通知

--- app/src/main/java/com/example/simplemusic/util/NotifyHelper.kt
+++ app/src/main/java/com/example/simplemusic/util/NotifyHelper.kt
@@ -3,10 +3,17 @@ package com.example.simplemusic.util
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.simplemusic.App.Companion.appContext
+import com.example.simplemusic.R
+import com.example.simplemusic.activity.PlayerActivity
+
 
 object NotifyHelper {
     private var notification: NotificationCompat.Builder? = null
@@ -16,11 +23,10 @@ object NotifyHelper {
 
     init {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(notificationChannelId, notificationChannelName, NotificationManager.IMPORTANCE_HIGH).apply {
-                this.setShowBadge(true)
-                this.enableLights(true)
-                this.lockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE
-                this.setBypassDnd(true)
+            NotificationChannel(notificationChannelId, notificationChannelName, NotificationManager.IMPORTANCE_MIN).apply {
+                this.setShowBadge(false)
+                this.enableLights(false)
+                this.setBypassDnd(false)
                 notificationManager.createNotificationChannel(this)
             }
         }
@@ -28,12 +34,16 @@ object NotifyHelper {
 
     @JvmStatic
     fun notify(title: String, artist: String): Notification {
+        val contentIntent = PendingIntent.getActivity(appContext, 0, Intent(appContext, PlayerActivity::class.java), 0)
         notification = notification ?: NotificationCompat.Builder(appContext, notificationChannelId)
-                .setContentTitle(title)
+        notification!!.setContentTitle(title)
                 .setContentText(artist)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_PROGRESS)
+                .setDefaults(NotificationCompat.FLAG_ONLY_ALERT_ONCE)
+                .setSmallIcon(R.drawable.defult_music_img)
                 .setOngoing(true)
+                .setContentIntent(contentIntent)
         val build = notification!!.build()
         notificationManager.notify(999, build)
         return build
